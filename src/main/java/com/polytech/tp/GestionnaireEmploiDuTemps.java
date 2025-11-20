4b735582f46e0adc66daa88c2c8d08955d7bebb5
yyyyacine16 <yacinesales1@gmail.com>
2025-11-20 13:51:21 +0100

    empty commit

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -5,17 +5,33 @@ import java.util.List;
 
 public class GestionnaireEmploiDuTemps {
     private List<ICours> listeCours = new ArrayList<>();
-
+    private List<Observer> listeObservateurs = new ArrayList<>();
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        for(Observer o : listeObservateurs) {
+            o.update("Nouveau cours ajouté : " + cours.getDescription());
+        }
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
+        for (ICours c : listeCours) {
+            if (c.equals(cours)) {
+                listeCours.remove(c);
+                listeCours.add(cours);
+
+
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        for(Observer o : listeObservateurs) {
+            o.update("Cours modifié : " + cours.getDescription());
+        }
+
+            }
+        }
+       System.out.println("cours non trouvé");
     }
 
     public void setChangement(String string) {
